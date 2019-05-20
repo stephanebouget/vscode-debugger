bd160119832c24edd60cc47c202dfa0b18f4cda7
Stephane Bouget <stephane.bouget@orange.com>
2019-05-20 15:42:34 +0200

    Add shortcut to add keyword.

--- out/extension.js
+++ out/extension.js
@@ -8,14 +8,23 @@ const vscode = require("vscode");
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-debugger" is now active!');
+    console.log('debugger added !');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+    let disposable = vscode.commands.registerCommand('vscodedebugger.debugger', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        // vscode.window.showInformationMessage('debugger World!');
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        const selection = editor.selection;
+        const lineOfSelectedVar = selection.active.line;
+        editor.edit(editBuilder => {
+            editBuilder.insert(new vscode.Position(lineOfSelectedVar, 0), 'debugger');
+        });
     });
     context.subscriptions.push(disposable);
 }
